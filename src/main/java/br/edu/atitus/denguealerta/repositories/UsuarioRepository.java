8ff27bffb9e2e3b702275754b67edf9c132d2dab
HenriqueZanfirAssis <151584480+HenriqueZanfirAssis@users.noreply.github.com>
2024-06-25 05:03:03 -0300

    Add files via upload

---
+++ src/main/java/br/edu/atitus/denguealerta/repositories/UsuarioRepository.java
@@ -0,0 +1,19 @@
+package br.edu.atitus.denguealerta.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import br.edu.atitus.denguealerta.entities.UsuarioEntity;
+
+public interface UsuarioRepository extends GenericRepository<UsuarioEntity> {
+
+	boolean existsByEmail(String email);
+	
+	boolean existsByCpf(String cpf);
+	
+	boolean existsByEmailAndIdNot(String email, UUID id);
+	
+	boolean existsByCpfAndIdNot(String cpf, UUID id);
+	
+	Optional<UsuarioEntity> findByEmail(String email);
+}
